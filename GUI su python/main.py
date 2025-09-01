471767eacd1db0569588ab6f07139b6a7b0e58ea
francescobifulco <francescobifulco362@gmail.com>
2025-09-01 20:37:01 +0200

    nuovo

---
+++ GUI su python/main.py
@@ -0,0 +1,11 @@
+# inporto le liberie di tkinter
+from tkinter import *
+from tkinter import ttk
+
+root = Tk() #mi crea la finestra ma chiude subito
+
+root.title('il nostro programma') #definiama il titolo della finesta
+root.geometry('600x400+500+500') #le dimensioni della finestra
+root.iconbitmap('internet.ico') #imposto la mia icona
+
+root.mainloop() #mi fa mantenere la finestra aperta 
