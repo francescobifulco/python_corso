fd937ff733c80e115e602b519035528c407dc2f5
francescobifulco <francescobifulco362@gmail.com>
2025-09-09 19:30:26 +0200

    nuovo

--- web con python flask/main.py
+++ web con python flask/main.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  # Crea l'app Flask
 
-@app.route("/")        # Definisce una route per la homepage
+@app.route('/')        # Definisce una route per la homepage
 def hello():
     return render_template('index.html')
